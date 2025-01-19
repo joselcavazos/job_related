9a9829117ed8bdac7ee5f5bafbd515676668af2d
Jose Cavazos <41312858+joselcavazos@users.noreply.github.com>
2025-01-18 23:06:28 -0600

    update 1/18

--- output_snowflake.sql
+++ output_snowflake.sql
@@ -1,36 +1,108 @@
-WITH IC AS
-  (SELECT t.sfid,
-          t.name,
-          t.mastercarves__c,
-          CONCAT(COALESCE(m.name, ''), COALESCE('(', ''), COALESCE(m.sfid, ''), COALESCE(');', '')) AS KEY,
-          m.name AS MC,
-          m.sfid AS MC_ID,
-          IFF(_u.pos = _u_2.pos_2, _u_2.unique_MC, NULL) AS unique_MC,
-          t.parent_territory__c
-   FROM EDH_PRD.SHARED.EOS_TERRITORY__C AS t
-   LEFT JOIN EDH_PRD.SHARED.EOS_MASTER_CARVE__C AS m ON t.master_carve__c = m.sfid
-   CROSS JOIN TABLE(FLATTEN(INPUT => ARRAY_GENERATE_RANGE(0, (GREATEST(ARRAY_SIZE(STRING_TO_ARRAY(t.mastercarves__c, ';'))) - 1) + 1))) AS _u(seq, KEY, PATH, INDEX, pos, this)
-   CROSS JOIN TABLE(FLATTEN(INPUT => STRING_TO_ARRAY(t.mastercarves__c, ';'))) AS _u_2(seq, KEY, PATH, pos_2, unique_MC, this)
-   WHERE t.territory_classification__c = 'Core AE'
+/* VAL 28 */ WITH Setting_Auto_Act_EOS AS
+  (SELECT name,
+          IFF(_u.pos = _u_2.pos_2, _u_2.value, NULL) AS value
+   FROM EDH_PRD.SHARED.EOS_TEAM_SETTINGS__C
+   CROSS JOIN TABLE(FLATTEN(INPUT => ARRAY_GENERATE_RANGE(0, (GREATEST(ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';'))) - 1) + 1))) AS _u(seq, KEY, PATH, INDEX, pos, this)
+   CROSS JOIN TABLE(FLATTEN(INPUT => STRING_TO_ARRAY(active_team_roles__c, ';'))) AS _u_2(seq, KEY, PATH, pos_2, value, this)
+   WHERE name IN ('AutomatedAccountModelTeamSettings')
      AND (_u.pos = _u_2.pos_2
-          OR (_u.pos > (ARRAY_SIZE(STRING_TO_ARRAY(t.mastercarves__c, ';')) - 1)
-              AND _u_2.pos_2 = (ARRAY_SIZE(STRING_TO_ARRAY(t.mastercarves__c, ';')) - 1))))
-SELECT CASE
-           WHEN NOT t1.mastercarves__c LIKE '%' || t1.key || '%'
-                AND NOT t1.MC IS NULL THEN 'Child issue'
-           WHEN NOT t2.mastercarves__c LIKE '%' || t1.unique_MC || '%' THEN CONCAT(COALESCE('Value Missing on the Parent : ', ''), COALESCE(t1.unique_MC, ''))
-       END AS Tops_Comment,
-       t1.sfid AS "Child Territory ID",
-       t1.name AS "Child Territory Name",
-       t1.mastercarves__c AS "Child Territory Master Carve Field",
-       t1.MC AS "Child Master Carve Name",
-       t1.MC_ID AS "Child Master Carve ID",
-       t2.name AS "Parent Territory Name",
-       t2.mastercarves__c AS "Parent Territory Master Carve Field",
-       t1.unique_MC,
-       t1.key
-FROM IC AS t1
-JOIN EDH_PRD.SHARED.EOS_TERRITORY__C AS t2 ON t1.parent_territory__c = t2.sfid
-WHERE ((NOT t1.mastercarves__c LIKE '%' || t1.key || '%'
-        AND NOT t1.MC IS NULL)
-       OR NOT t2.mastercarves__c LIKE '%' || t1.unique_MC || '%')
+          OR (_u.pos > (ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';')) - 1)
+              AND _u_2.pos_2 = (ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';')) - 1)))),
+                  Setting_Auto_Co_EOS AS
+  (SELECT name,
+          IFF(_u.pos = _u_2.pos_2, _u_2.value, NULL) AS value
+   FROM EDH_PRD.SHARED.EOS_TEAM_SETTINGS__C
+   CROSS JOIN TABLE(FLATTEN(INPUT => ARRAY_GENERATE_RANGE(0, (GREATEST(ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';'))) - 1) + 1))) AS _u(seq, KEY, PATH, INDEX, pos, this)
+   CROSS JOIN TABLE(FLATTEN(INPUT => STRING_TO_ARRAY(co_prime_territories__c, ';'))) AS _u_2(seq, KEY, PATH, pos_2, value, this)
+   WHERE name IN ('AutomatedAccountModelTeamSettings')
+     AND (_u.pos = _u_2.pos_2
+          OR (_u.pos > (ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';')) - 1)
+              AND _u_2.pos_2 = (ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';')) - 1)))),
+                  Setting_Team_Act_EOS AS
+  (SELECT name,
+          IFF(_u.pos = _u_2.pos_2, _u_2.value, NULL) AS value
+   FROM EDH_PRD.SHARED.EOS_TEAM_SETTINGS__C
+   CROSS JOIN TABLE(FLATTEN(INPUT => ARRAY_GENERATE_RANGE(0, (GREATEST(ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';'))) - 1) + 1))) AS _u(seq, KEY, PATH, INDEX, pos, this)
+   CROSS JOIN TABLE(FLATTEN(INPUT => STRING_TO_ARRAY(active_team_roles__c, ';'))) AS _u_2(seq, KEY, PATH, pos_2, value, this)
+   WHERE name IN ('Team Account Model Settings')
+     AND (_u.pos = _u_2.pos_2
+          OR (_u.pos > (ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';')) - 1)
+              AND _u_2.pos_2 = (ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';')) - 1)))),
+                  Setting_Team_Co_EOS AS
+  (SELECT name,
+          IFF(_u.pos = _u_2.pos_2, _u_2.value, NULL) AS value
+   FROM EDH_PRD.SHARED.EOS_TEAM_SETTINGS__C
+   CROSS JOIN TABLE(FLATTEN(INPUT => ARRAY_GENERATE_RANGE(0, (GREATEST(ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';'))) - 1) + 1))) AS _u(seq, KEY, PATH, INDEX, pos, this)
+   CROSS JOIN TABLE(FLATTEN(INPUT => STRING_TO_ARRAY(co_prime_territories__c, ';'))) AS _u_2(seq, KEY, PATH, pos_2, value, this)
+   WHERE name IN ('Team Account Model Settings')
+     AND (_u.pos = _u_2.pos_2
+          OR (_u.pos > (ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';')) - 1)
+              AND _u_2.pos_2 = (ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';')) - 1)))),
+                  team_eos AS
+  (SELECT name
+   FROM EDH_PRD.SHARED.EOS_TEAM_ROLE__C
+   WHERE LEFT(territory_usage_type__c, 15) = 'a1td00000000WFY'
+     AND active__c = TRUE)
+SELECT 'To be Removed from Team Setting' AS "Comment",
+       eos.name AS "Records",
+       'Active Team Role' AS "Field",
+       eos.value
+FROM Setting_Auto_Act_EOS AS eos
+LEFT JOIN team_eos AS team ON eos.value = team.name
+WHERE team.name IS NULL
+UNION
+SELECT 'To be Added in Team Setting',
+       'AutomatedAccountModelTeamSettings',
+       'Active Team Role',
+       team.name
+FROM team_eos AS team
+LEFT JOIN Setting_Auto_Act_EOS AS eos ON eos.value = team.name
+WHERE eos.name IS NULL
+UNION
+SELECT 'To be Removed from Team Setting' AS "Comment",
+       eos.name,
+       'Co-Prime Territories',
+       eos.value
+FROM Setting_Auto_Co_EOS AS eos
+LEFT JOIN team_eos AS team ON eos.value = team.name
+WHERE team.name IS NULL
+UNION
+SELECT 'To be Added in Team Setting',
+       'AutomatedAccountModelTeamSettings',
+       'Co-Prime Territories',
+       team.name
+FROM team_eos AS team
+LEFT JOIN Setting_Auto_Co_EOS AS eos ON eos.value = team.name
+WHERE eos.name IS NULL
+UNION
+SELECT 'To be Removed from Team Setting' AS "Comment",
+       eos.name AS "Records",
+       'Active Team Role',
+       eos.value
+FROM Setting_Team_Act_EOS AS eos
+LEFT JOIN team_eos AS team ON eos.value = team.name
+WHERE team.name IS NULL
+UNION
+SELECT 'To be Added in Team Setting',
+       'Team Account Model Settings',
+       'Active Team Role',
+       team.name
+FROM team_eos AS team
+LEFT JOIN Setting_Team_Act_EOS AS eos ON eos.value = team.name
+WHERE eos.name IS NULL
+UNION
+SELECT 'To be Removed from Team Setting' AS "Comment",
+       eos.name,
+       'Co-Prime Territories',
+       eos.value
+FROM Setting_Team_Co_EOS AS eos
+LEFT JOIN team_eos AS team ON eos.value = team.name
+WHERE team.name IS NULL
+UNION
+SELECT 'To be Added in Team Setting',
+       'Team Account Model Settings',
+       'Co-Prime Territories',
+       team.name
+FROM team_eos AS team
+LEFT JOIN Setting_Team_Co_EOS AS eos ON eos.value = team.name
+WHERE eos.name IS NULL
