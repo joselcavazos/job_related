2d23fc448ab649bba8aae28cd7a3d1d902513429
Jose Cavazos <41312858+joselcavazos@users.noreply.github.com>
2025-02-23 14:08:21 -0600

    2/28
    2/28

--- .ipynb_checkpoints/input_postgres-checkpoint.sql
+++ .ipynb_checkpoints/input_postgres-checkpoint.sql
@@ -1,65 +1,52 @@
--- VAL 28
-
-with Setting_Auto_Act_EOS as (Select name,unnest(string_to_array(active_team_roles__c, ';')) as value
-from eospilot.team_settings__c
-where name in ('AutomatedAccountModelTeamSettings')),
-Setting_Auto_Co_EOS as (Select name,unnest(string_to_array(co_prime_territories__c, ';')) as value
-from eospilot.team_settings__c
-where name in ('AutomatedAccountModelTeamSettings')),
-Setting_Team_Act_EOS as (Select name,unnest(string_to_array(active_team_roles__c, ';')) as value
-from eospilot.team_settings__c
-where name in ('Team Account Model Settings')),
-Setting_Team_Co_EOS as (Select name,unnest(string_to_array(co_prime_territories__c, ';')) as value
-from eospilot.team_settings__c
-where name in ('Team Account Model Settings')),
-team_eos as (Select name
-from eospilot.team_role__c
-where left(territory_usage_type__c,15) = 'a1td00000000WFY' and active__c = TRUE )
-Select 'To be Removed from Team Setting' as "Comment",eos.name as "Records",'Active Team Role' as "Field",eos.value
-from Setting_Auto_Act_EOS eos
-left join team_eos team
-on eos.value = team.name
-where team.name is null
-UNION
-Select 'To be Added in Team Setting','AutomatedAccountModelTeamSettings','Active Team Role',team.name
-from team_eos team
-left join Setting_Auto_Act_EOS eos
-on eos.value = team.name
-where eos.name is null
-UNION
-Select 'To be Removed from Team Setting' as "Comment",eos.name,'Co-Prime Territories',eos.value
-from Setting_Auto_Co_EOS eos
-left join team_eos team
-on eos.value = team.name
-where team.name is null
-UNION
-Select 'To be Added in Team Setting','AutomatedAccountModelTeamSettings','Co-Prime Territories',team.name
-from team_eos team
-left join Setting_Auto_Co_EOS  eos
-on eos.value = team.name
-where eos.name is null
-UNION
-Select 'To be Removed from Team Setting' as "Comment",eos.name as "Records",'Active Team Role',eos.value
-from Setting_Team_Act_EOS eos
-left join team_eos team
-on eos.value = team.name
-where team.name is null
-UNION
-Select 'To be Added in Team Setting','Team Account Model Settings','Active Team Role',team.name
-from team_eos team
-left join Setting_Team_Act_EOS eos
-on eos.value = team.name
-where eos.name is null
-UNION
-Select 'To be Removed from Team Setting' as "Comment",eos.name,'Co-Prime Territories',eos.value
-from Setting_Team_Co_EOS eos
-left join team_eos team
-on eos.value = team.name
-where team.name is null
-UNION
-Select 'To be Added in Team Setting','Team Account Model Settings','Co-Prime Territories',team.name
-from  team_eos team
-left join Setting_Team_Co_EOS eos
-on eos.value = team.name
-where eos.name is null
-
+With OU as (select distinct sfid,name,unnest(string_to_array(operating_units__c, ';')) as unique_ou from eospilot.team_role__c where active__c = TRUE)
+select distinct
+CASE
+  When Role_Org62.sfid is null then 'Create Team Role in Org62'
+  When Role_EOS.name != Role_Org62.name then 'Update Team Role Name after Validation Rule is desactivate'
+  ELSE 'Update Team Role in Org62 based on EOS'
+End as TOPS_Comment,
+  Role_EOS.sfid as EOS_Team_Role_ID,
+  Role_Org62.sfid as Org62_Team_Role_ID,
+  Role_EOS.name as EOS_Team_Role_Name,
+  Role_Org62.name as Org62_Team_Role_Name,
+  Role_EOS.sellingrolegroup__c as EOS_Selling_Role_Group,
+  Role_Org62.sellingrolegroup__c as Org62_Selling_Role_Group,
+  Role_EOS.operating_units__c as EOS_Team_Role_Operating_Units,
+  Role_Org62.operating_units__c as Org62_Team_Role_Operating_Units,
+  Role_EOS.group__c as EOS_Team_Role_Group,
+  Role_Org62.group__c as Org62_Team_Role_Group,
+  role_eos.exclude_role_reasons__c as EOS_Team_Role_Exclude_Role_Reasons,
+  Role_Org62.excluderolereasons__c as Org62_Team_Role_Exclude_Role_Reasons,
+  Role_EOS.mastercarveenabled__c as EOS_Master_Carve_Enabled,
+  Role_Org62.mastercarveenabled__c as Org62_Master_Carve_Enabled,
+  Role_EOS.following__c as EOS_Team_Role_Following,
+  Role_Org62.following__c as Org62_Team_Role_Following,
+  Role_EOS.prefix__c as EOS_Team_Role_Prefix,
+  Role_Org62.prefix__c as Org62_Team_Role_Prefix,
+  CASE
+    when Role_EOS.territory_usage_type__c = 'a1td00000000WFYAA2' then 'Sales'
+    when Role_EOS.territory_usage_type__c = 'a1t0W000003kut2QAA' then 'Non Carved'
+  End as EOS_Team_Role_Territory_Usage_Type,
+  CASE
+    when Role_Org62.Territory_Usage_Type__c = 'aFF300000004C93GAE' then 'Sales'
+    when Role_Org62.Territory_Usage_Type__c = 'aFF0M000000CaRHWA0' then 'Non Carved'
+  End as Org62_Team_Role_Territory_Usage_Type
+from (select * from eospilot.team_role__c where active__c = TRUE) as Role_EOS
+LEFT JOIN org62.team_role__c as Role_Org62
+  on Role_EOS.planning_org_xref__c = Role_Org62.planning_org_xref__c
+Left JOIN OU ou
+  on Role_EOS.sfid = ou.sfid
+where (Role_Org62.sfid is null and Role_EOS.active__c is TRUE) or
+   coalesce(Role_EOS.name,'') != coalesce(Role_Org62.name,'') or
+--   (Role_EOS.active__c = true and Role_Org62.active__c = false) or    -- Leave previous year Team Role active
+   coalesce(Role_EOS.following__c,'') != coalesce(Role_Org62.following__c,'') or
+ --  coalesce(Role_EOS.operating_units__c,'') != coalesce(Role_Org62.operating_units__c,'') or
+   coalesce(Role_Org62.operating_units__c,'') not LIKE '%'|| unique_ou ||'%' or
+   coalesce(Role_EOS.group__c,'') != coalesce(Role_Org62.group__c,'') or
+   coalesce(role_eos.exclude_role_reasons__c,'') != coalesce(Role_Org62.excluderolereasons__c,'') or
+   coalesce(Role_EOS.prefix__c,'') != coalesce(Role_Org62.prefix__c,'') or
+   coalesce(Role_EOS.sellingrolegroup__c,'') != coalesce(Role_Org62.sellingrolegroup__c,'') or
+   Role_EOS.mastercarveenabled__c != Role_Org62.mastercarveenabled__c or
+      (Role_EOS.territory_usage_type__c = 'a1td00000000WFYAA2' and Role_Org62.Territory_Usage_Type__c = 'aFF0M000000CaRHWA0') or
+      (Role_EOS.territory_usage_type__c = 'a1t0W000003kut2QAA' and Role_Org62.Territory_Usage_Type__c = 'aFF300000004C93GAE')
+      
--- output_snowflake.sql
+++ output_snowflake.sql
@@ -1,108 +1,62 @@
-/* VAL 28 */ WITH Setting_Auto_Act_EOS AS
-  (SELECT name,
-          IFF(_u.pos = _u_2.pos_2, _u_2.value, NULL) AS value
-   FROM EDH_PRD.SHARED.EOS_TEAM_SETTINGS__C
-   CROSS JOIN TABLE(FLATTEN(INPUT => ARRAY_GENERATE_RANGE(0, (GREATEST(ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';'))) - 1) + 1))) AS _u(seq, KEY, PATH, INDEX, pos, this)
-   CROSS JOIN TABLE(FLATTEN(INPUT => STRING_TO_ARRAY(active_team_roles__c, ';'))) AS _u_2(seq, KEY, PATH, pos_2, value, this)
-   WHERE name IN ('AutomatedAccountModelTeamSettings')
-     AND (_u.pos = _u_2.pos_2
-          OR (_u.pos > (ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';')) - 1)
-              AND _u_2.pos_2 = (ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';')) - 1)))),
-                  Setting_Auto_Co_EOS AS
-  (SELECT name,
-          IFF(_u.pos = _u_2.pos_2, _u_2.value, NULL) AS value
-   FROM EDH_PRD.SHARED.EOS_TEAM_SETTINGS__C
-   CROSS JOIN TABLE(FLATTEN(INPUT => ARRAY_GENERATE_RANGE(0, (GREATEST(ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';'))) - 1) + 1))) AS _u(seq, KEY, PATH, INDEX, pos, this)
-   CROSS JOIN TABLE(FLATTEN(INPUT => STRING_TO_ARRAY(co_prime_territories__c, ';'))) AS _u_2(seq, KEY, PATH, pos_2, value, this)
-   WHERE name IN ('AutomatedAccountModelTeamSettings')
-     AND (_u.pos = _u_2.pos_2
-          OR (_u.pos > (ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';')) - 1)
-              AND _u_2.pos_2 = (ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';')) - 1)))),
-                  Setting_Team_Act_EOS AS
-  (SELECT name,
-          IFF(_u.pos = _u_2.pos_2, _u_2.value, NULL) AS value
-   FROM EDH_PRD.SHARED.EOS_TEAM_SETTINGS__C
-   CROSS JOIN TABLE(FLATTEN(INPUT => ARRAY_GENERATE_RANGE(0, (GREATEST(ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';'))) - 1) + 1))) AS _u(seq, KEY, PATH, INDEX, pos, this)
-   CROSS JOIN TABLE(FLATTEN(INPUT => STRING_TO_ARRAY(active_team_roles__c, ';'))) AS _u_2(seq, KEY, PATH, pos_2, value, this)
-   WHERE name IN ('Team Account Model Settings')
-     AND (_u.pos = _u_2.pos_2
-          OR (_u.pos > (ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';')) - 1)
-              AND _u_2.pos_2 = (ARRAY_SIZE(STRING_TO_ARRAY(active_team_roles__c, ';')) - 1)))),
-                  Setting_Team_Co_EOS AS
-  (SELECT name,
-          IFF(_u.pos = _u_2.pos_2, _u_2.value, NULL) AS value
-   FROM EDH_PRD.SHARED.EOS_TEAM_SETTINGS__C
-   CROSS JOIN TABLE(FLATTEN(INPUT => ARRAY_GENERATE_RANGE(0, (GREATEST(ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';'))) - 1) + 1))) AS _u(seq, KEY, PATH, INDEX, pos, this)
-   CROSS JOIN TABLE(FLATTEN(INPUT => STRING_TO_ARRAY(co_prime_territories__c, ';'))) AS _u_2(seq, KEY, PATH, pos_2, value, this)
-   WHERE name IN ('Team Account Model Settings')
-     AND (_u.pos = _u_2.pos_2
-          OR (_u.pos > (ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';')) - 1)
-              AND _u_2.pos_2 = (ARRAY_SIZE(STRING_TO_ARRAY(co_prime_territories__c, ';')) - 1)))),
-                  team_eos AS
-  (SELECT name
+WITH OU AS
+  (SELECT DISTINCT ID,
+                   name,
+                   IFF(_u.pos = _u_2.pos_2, _u_2.unique_ou, NULL) AS unique_ou
    FROM EDH_PRD.SHARED.EOS_TEAM_ROLE__C
-   WHERE LEFT(territory_usage_type__c, 15) = 'a1td00000000WFY'
-     AND active__c = TRUE)
-SELECT 'To be Removed from Team Setting' AS "Comment",
-       eos.name AS "Records",
-       'Active Team Role' AS "Field",
-       eos.value
-FROM Setting_Auto_Act_EOS AS eos
-LEFT JOIN team_eos AS team ON eos.value = team.name
-WHERE team.name IS NULL
-UNION
-SELECT 'To be Added in Team Setting',
-       'AutomatedAccountModelTeamSettings',
-       'Active Team Role',
-       team.name
-FROM team_eos AS team
-LEFT JOIN Setting_Auto_Act_EOS AS eos ON eos.value = team.name
-WHERE eos.name IS NULL
-UNION
-SELECT 'To be Removed from Team Setting' AS "Comment",
-       eos.name,
-       'Co-Prime Territories',
-       eos.value
-FROM Setting_Auto_Co_EOS AS eos
-LEFT JOIN team_eos AS team ON eos.value = team.name
-WHERE team.name IS NULL
-UNION
-SELECT 'To be Added in Team Setting',
-       'AutomatedAccountModelTeamSettings',
-       'Co-Prime Territories',
-       team.name
-FROM team_eos AS team
-LEFT JOIN Setting_Auto_Co_EOS AS eos ON eos.value = team.name
-WHERE eos.name IS NULL
-UNION
-SELECT 'To be Removed from Team Setting' AS "Comment",
-       eos.name AS "Records",
-       'Active Team Role',
-       eos.value
-FROM Setting_Team_Act_EOS AS eos
-LEFT JOIN team_eos AS team ON eos.value = team.name
-WHERE team.name IS NULL
-UNION
-SELECT 'To be Added in Team Setting',
-       'Team Account Model Settings',
-       'Active Team Role',
-       team.name
-FROM team_eos AS team
-LEFT JOIN Setting_Team_Act_EOS AS eos ON eos.value = team.name
-WHERE eos.name IS NULL
-UNION
-SELECT 'To be Removed from Team Setting' AS "Comment",
-       eos.name,
-       'Co-Prime Territories',
-       eos.value
-FROM Setting_Team_Co_EOS AS eos
-LEFT JOIN team_eos AS team ON eos.value = team.name
-WHERE team.name IS NULL
-UNION
-SELECT 'To be Added in Team Setting',
-       'Team Account Model Settings',
-       'Co-Prime Territories',
-       team.name
-FROM team_eos AS team
-LEFT JOIN Setting_Team_Co_EOS AS eos ON eos.value = team.name
-WHERE eos.name IS NULL
+   CROSS JOIN TABLE(FLATTEN(INPUT => ARRAY_GENERATE_RANGE(0, (GREATEST(ARRAY_SIZE(STRING_TO_ARRAY(operating_units__c, ';'))) - 1) + 1))) AS _u(seq, KEY, PATH, INDEX, pos, this)
+   CROSS JOIN TABLE(FLATTEN(INPUT => STRING_TO_ARRAY(operating_units__c, ';'))) AS _u_2(seq, KEY, PATH, pos_2, unique_ou, this)
+   WHERE active__c = TRUE
+     AND (_u.pos = _u_2.pos_2
+          OR (_u.pos > (ARRAY_SIZE(STRING_TO_ARRAY(operating_units__c, ';')) - 1)
+              AND _u_2.pos_2 = (ARRAY_SIZE(STRING_TO_ARRAY(operating_units__c, ';')) - 1))))
+SELECT DISTINCT CASE
+                    WHEN Role_Org62.ID IS NULL THEN 'Create Team Role in Org62'
+                    WHEN Role_EOS.name <> Role_Org62.name THEN 'Update Team Role Name after Validation Rule is desactivate'
+                    ELSE 'Update Team Role in Org62 based on EOS'
+                END AS TOPS_Comment,
+                Role_EOS.ID AS EOS_Team_Role_ID,
+                Role_Org62.ID AS Org62_Team_Role_ID,
+                Role_EOS.name AS EOS_Team_Role_Name,
+                Role_Org62.name AS Org62_Team_Role_Name,
+                Role_EOS.sellingrolegroup__c AS EOS_Selling_Role_Group,
+                Role_Org62.sellingrolegroup__c AS Org62_Selling_Role_Group,
+                Role_EOS.operating_units__c AS EOS_Team_Role_Operating_Units,
+                Role_Org62.operating_units__c AS Org62_Team_Role_Operating_Units,
+                Role_EOS.group__c AS EOS_Team_Role_Group,
+                Role_Org62.group__c AS Org62_Team_Role_Group,
+                role_eos.exclude_role_reasons__c AS EOS_Team_Role_Exclude_Role_Reasons,
+                Role_Org62.excluderolereasons__c AS Org62_Team_Role_Exclude_Role_Reasons,
+                Role_EOS.mastercarveenabled__c AS EOS_Master_Carve_Enabled,
+                Role_Org62.mastercarveenabled__c AS Org62_Master_Carve_Enabled,
+                Role_EOS.following__c AS EOS_Team_Role_Following,
+                Role_Org62.following__c AS Org62_Team_Role_Following,
+                Role_EOS.prefix__c AS EOS_Team_Role_Prefix,
+                Role_Org62.prefix__c AS Org62_Team_Role_Prefix,
+                CASE
+                    WHEN Role_EOS.territory_usage_type__c = 'a1td00000000WFYAA2' THEN 'Sales'
+                    WHEN Role_EOS.territory_usage_type__c = 'a1t0W000003kut2QAA' THEN 'Non Carved'
+                END AS EOS_Team_Role_Territory_Usage_Type,
+                CASE
+                    WHEN Role_Org62.Territory_Usage_Type__c = 'aFF300000004C93GAE' THEN 'Sales'
+                    WHEN Role_Org62.Territory_Usage_Type__c = 'aFF0M000000CaRHWA0' THEN 'Non Carved'
+                END AS Org62_Team_Role_Territory_Usage_Type
+FROM
+  (SELECT *
+   FROM EDH_PRD.SHARED.EOS_TEAM_ROLE__C
+   WHERE active__c = TRUE) AS Role_EOS
+LEFT JOIN SSE_PRD.ODS.ORG62_TEAM_ROLE__C AS Role_Org62 ON Role_EOS.planning_org_xref__c = Role_Org62.planning_org_xref__c
+LEFT JOIN OU AS ou ON Role_EOS.ID = ou.ID
+WHERE (Role_Org62.ID IS NULL
+       AND Role_EOS.active__c IS TRUE)
+  OR COALESCE(Role_EOS.name, '') <> COALESCE(Role_Org62.name, '')
+  OR COALESCE(Role_EOS.following__c, '') /*   (Role_EOS.active__c = true and Role_Org62.active__c = false) or    -- Leave previous year Team Role active */ <> COALESCE(Role_Org62.following__c, '')
+  OR NOT COALESCE(Role_Org62.operating_units__c, '') /*  coalesce(Role_EOS.operating_units__c,'') != coalesce(Role_Org62.operating_units__c,'') or */ LIKE '%' || unique_ou || '%'
+  OR COALESCE(Role_EOS.group__c, '') <> COALESCE(Role_Org62.group__c, '')
+  OR COALESCE(role_eos.exclude_role_reasons__c, '') <> COALESCE(Role_Org62.excluderolereasons__c, '')
+  OR COALESCE(Role_EOS.prefix__c, '') <> COALESCE(Role_Org62.prefix__c, '')
+  OR COALESCE(Role_EOS.sellingrolegroup__c, '') <> COALESCE(Role_Org62.sellingrolegroup__c, '')
+  OR Role_EOS.mastercarveenabled__c <> Role_Org62.mastercarveenabled__c
+  OR (Role_EOS.territory_usage_type__c = 'a1td00000000WFYAA2'
+      AND Role_Org62.Territory_Usage_Type__c = 'aFF0M000000CaRHWA0')
+  OR (Role_EOS.territory_usage_type__c = 'a1t0W000003kut2QAA'
+      AND Role_Org62.Territory_Usage_Type__c = 'aFF300000004C93GAE')
